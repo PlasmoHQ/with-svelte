0701fd8a424938b236f8ee42cbcc4591b388081e
Louis <6723574+louisgv@users.noreply.github.com>
2022-07-12 14:38:48 -0400

    Svelte popup working - most should work as expected

---
+++ popup.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let count = 0;
+    let action: string = null;
+    function increment() {
+        count += 1;
+        action = "increment";
+    }
+    function decrement() {
+        count -= 1;
+        action = "decrement";
+    }
+</script>
+
+<style>
+    .container {
+        min-width: 470px;
+    }
+    .action {
+        color: #470;
+        font-weight: bold;
+    }
+</style>
+
+<div class="container">
+    <p>Current count: <b>{count}</b></p>
+    <div>
+        <button on:click={decrement}>-</button>
+        <button on:click={increment}>+</button>
+        {#if action}<span class="action">{action}</span>{/if}
+    </div>
+</div>
